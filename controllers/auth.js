63498239bf037faccf10899fb48e1f4866c1cd89
willylikestokayak <wccoker@gmail.com>
2017-10-04 10:48:20 -0700

    first commit, auth boiler plate added

---
+++ controllers/auth.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+var passport = require('../config/ppConfig');
+
+router.get('/signup', function(req, res) {
+  res.render('auth/signup');
+});
+
+router.post('/signup', function(req, res) {
+  // find or create a user, providing the name and password as default values
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    if (created) {
+      // replace the contents of this if statement with the code below
+      passport.authenticate('local', {
+        successRedirect: '/',
+        successFlash: 'Account created and logged in'
+      })(req, res);
+    } else {
+      // if not created, the email already exists
+      req.flash('error', 'Email already exists');
+      res.redirect('/auth/signup');
+    }
+  }).catch(function(error) {
+    // if an error occurs, let's see what the error is
+    req.flash('error', error.message);
+    res.redirect('/auth/signup');
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  failureFlash: 'Invalid username and/or password',
+  successFlash: 'You have logged in'
+}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  req.flash('success', 'You have logged out');
+  res.redirect('/');
+});
+
+module.exports = router;
